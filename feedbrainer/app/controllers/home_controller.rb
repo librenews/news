629789d3b516d842847683aac11df7b8adad95c7
Matthew Terenzio <mterenzio@gmail.com>
2025-12-04 07:46:50 -0500

    adds time decay to home page articles

--- feedbrainer/app/controllers/home_controller.rb
+++ feedbrainer/app/controllers/home_controller.rb
@@ -1,11 +1,15 @@
 class HomeController < ApplicationController
   def index
-    # Most popular articles ranked by number of shares (ArticlePosts)
+    # Hacker News ranking algorithm: (p - 1) / (t + 2)^1.8
+    # We use share_count as 'p' and hours since creation as 't'
+    gravity = 1.8
+    
     @articles = Article
       .joins(:article_posts)
+      .where("articles.created_at > ?", 7.days.ago)
       .group("articles.id")
       .select("articles.*, COUNT(article_posts.id) AS share_count")
-      .order("COUNT(article_posts.id) DESC, articles.created_at DESC")
+      .order(Arel.sql("COUNT(article_posts.id) / POWER((EXTRACT(EPOCH FROM (NOW() - articles.created_at)) / 3600) + 2, #{gravity}) DESC"))
       .limit(50)
       .includes(posts: :source)
   end
